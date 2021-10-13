a3d4acbc4ea2ba7fc14e1864c66d76f73b1517e2
harveybegood <harveymorning@gmail.com>
2021-10-12 23:22:17 -0700

    upload longtest_line_with_external_var

---
+++ longest_line_with_external_var.c
@@ -0,0 +1,83 @@
+/*
+ *	rewrite the longest-line program with line, longest, and max as external variables.
+ *	
+ *	Mainly focus on that declaration may be an explicit extern statement or may be 
+ *	implicit from context.
+ *
+ * */
+
+#include <stdio.h>
+#include "externhead.h" // implicit extern statement
+
+// explicit extern statement as below
+
+//#define MAXLINE 1000
+
+//int max;
+//char line[MAXLINE];
+//char longest[MAXLINE];
+
+int get_line(void);
+void copy(void);
+
+int main(){
+
+	int len;
+	/*extern int max;
+	extern char longest[]; 
+
+	*/
+	extern int max;
+    extern char longest[]; 
+	max = 0;
+	while ((len = get_line()) > 0){
+	
+		if (len > max){
+			max = len;
+			copy();
+		}
+	}
+
+	if (max > 0){
+	
+		printf("%s", longest);
+	}
+	
+	return 0;
+}
+
+int get_line(void){
+	int i, c;
+
+	extern char line[]; 
+
+	// utilize getchar() to get each next line if there is any
+	
+	for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; ++i){
+		
+		line[i] = c;
+	}
+
+	if (c == '\n'){
+
+		line[i] = c;
+		++i;
+	}
+
+	line[i] = '\0';
+
+	return i;
+}
+
+void copy(void){
+	int i;
+	
+	extern char line[];
+	extern char longest[];
+
+	i = 0;	
+	while ((longest[i] = line[i]) != '\0'){
+		
+		++i;
+	}
+}
